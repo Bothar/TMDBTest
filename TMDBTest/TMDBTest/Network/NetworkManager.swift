157444b5cbc9e41bbe89c07a6b016eede34a43f0
David Miguel Martinez <dmiguema10@alumnes.ub.edu>
2018-05-06 11:44:05 +0200

    -Base project

---
+++ TMDBTest/TMDBTest/Network/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  TMDBTest
+//
+//  Created by David Miguel Martinez on 06/05/2018.
+//  Copyright © 2018 David Miguel Martinez. All rights reserved.
+//
+
+import Foundation
---
+++ TMDBTest/TMDBTest/UserStories/1.ListMovies/ListMoviesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMDBTest
+//
+//  Created by David Miguel Martinez on 06/05/2018.
+//  Copyright © 2018 David Miguel Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ListMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
